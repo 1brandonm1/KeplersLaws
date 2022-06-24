44a0c2c6708f8877ad27b37aaa9325dcebd3d260
Brandon Martinez <1brandonmartinez1@gmail.com>
2022-06-24 15:03:06 -0500

    Creates working 3D tab navigator

---
+++ Assets/Datafiles/Scripts/TabButton.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems; // to access IPointer
+
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    public TabGroup tabGroup;
+
+    public Image background;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.OnTabExit(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        background = GetComponent<Image>();
+        tabGroup.Subscribe(this);
+
+    }
+
+    public void Select()
+    {
+
+    }
+
+    public void Deselect()
+    {
+
+    }
+}
